a1b82bff5f233270c693b0973cc92d6f1f8bdbaf
Quang Pham <phvuquang@gmail.com>
2022-11-11 09:14:13 +0700

    Update

---
+++ src/main/java/GUI/MenuScence.java
@@ -0,0 +1,20 @@
+import GUI.Scence;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class MenuScence extends Scence {
+
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+
+    }
+}
