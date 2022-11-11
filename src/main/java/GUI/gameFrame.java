fab0694e1ce73e6a4c81d925fbd89b4a1f391b92
Trần Nguyên Phúc <itcsiu21097@student.hcmiu.edu.vn>
2022-11-11 15:15:19 +0700

    Update Image

--- src/main/java/GUI/gameFrame.java
+++ src/main/java/GUI/gameFrame.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 public class gameFrame extends JFrame {
     public gameFrame() {
         this.setTitle("Bomberman Adventure");
-        this.setIconImage(new ImageIcon("src\\main\\resources\\Bomb\\bomb1.png").getImage());
+        this.setIconImage(new ImageIcon("src\\main\\resources\\Logo-1.png").getImage());
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         gamePanel game = new gamePanel();
