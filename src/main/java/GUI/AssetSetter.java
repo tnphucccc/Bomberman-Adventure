1a6920543c90447295f412e2e23dbd4f681ee18e
Hueyyyy <khatihuy125@gmail.com>
2022-11-18 14:01:23 +0700

    add items to map

--- src/main/java/GUI/AssetSetter.java
+++ src/main/java/GUI/AssetSetter.java
@@ -1,6 +1,10 @@
 package GUI;
 
 import Entity.Mob;
+import Objects.OBJ_BlastRadius;
+import Objects.OBJ_SpeedIncrease;
+import Objects.SuperObject;
+import Variables.Constant;
 
 public class AssetSetter {
     GameScence gameScence;
@@ -11,6 +15,12 @@ public class AssetSetter {
     public void setMob(){
         gameScence.mob[0]=new Mob(gameScence,144,224);
         gameScence.mob[1]=new Mob(gameScence,624,32);
+        gameScence.Object[0] = new OBJ_BlastRadius();
+        gameScence.Object[0].x = 5* Constant.tileSize;
+        gameScence.Object[0].y = 1* Constant.tileSize;
+        gameScence.Object[1] = new OBJ_SpeedIncrease();
+        gameScence.Object[1].x = 5* Constant.tileSize;
+        gameScence.Object[1].y = 5* Constant.tileSize;
     }
     public void setItems(){}
 }
---
+++ src/main/java/Objects/SuperObject.java
@@ -0,0 +1,17 @@
+package Objects;
+
+import GUI.GameScence;
+import Variables.Constant;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class SuperObject {
+    public BufferedImage image;
+    public String name;
+    public boolean collision = false;
+    public int x,y;
+    public void draw (Graphics2D g2, GameScence gameScence){
+        g2.drawImage(image, x, y, Constant.tileSize, Constant.tileSize, null);
+    }
+}
