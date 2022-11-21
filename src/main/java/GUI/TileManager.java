139951154f5cfb7f7c7027b2ecc902fef1b0e2de
Trần Nguyên Phúc <itcsiu21097@student.hcmiu.edu.vn>
2022-11-21 18:02:15 +0700

    Bug fixing

--- src/main/java/GUI/TileManager.java
+++ src/main/java/GUI/TileManager.java
@@ -38,7 +38,7 @@ public class TileManager {
             tiles[3].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/Tiles/GroundShadow.png")));
 
             tiles[4] = new Tile();
-            tiles[4].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/Tiles/Sand.png")));
+            tiles[4].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/Tiles/sand.png")));
         } catch (IOException e) {
             e.printStackTrace();
         }
