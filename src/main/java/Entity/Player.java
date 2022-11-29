1e239392cae3be1c6affa62a134e7ea74f717dcd
Trần Nguyên Phúc <itcsiu21097@student.hcmiu.edu.vn>
2022-11-29 18:07:35 +0700

    Fix Player's SolidArea

--- src/main/java/Entity/Player.java
+++ src/main/java/Entity/Player.java
@@ -20,7 +20,7 @@ public class Player extends Entity {
         this.keyH = keyH;
         solidArea = new Rectangle();
         solidArea.x = 8;
-        solidArea.y = 8;
+        solidArea.y = 16;
         solidAreaDefaultX = solidArea.x;
         solidAreaDefaultY = solidArea.y;
         solidArea.width = 32;
