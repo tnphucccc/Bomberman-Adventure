cce0735c9c7320d550ca584d14ae0551d79726c3
Quang Pham <ititiu21096@student.hcmiu.edu.vn>
2022-12-03 00:54:34 +0700

    Merge with Master

--- src/main/java/GUI/TileManager.java
+++ src/main/java/GUI/TileManager.java
@@ -50,7 +50,7 @@ public class TileManager {
             tiles[3].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/Tiles/GroundShadow.png")));
 
             tiles[4] = new Tile();//Seriously ? Sand vs sand typo?
-            tiles[4].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/Tiles/sand.png")));
+            tiles[4].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/Tiles/Sand.png")));
 
         } catch (IOException e) {
             e.printStackTrace();
