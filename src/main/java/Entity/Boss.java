cc2af7377e8246aef2ed700fbe29e8f237bd5983
Nguyễn Mạch Khang Huy <itcsiu21072@student.hcmiu.edu.vn>
2022-12-29 00:40:03 +0700

    update map 2

--- src/main/java/Entity/Boss.java
+++ src/main/java/Entity/Boss.java
@@ -38,7 +38,7 @@ public class Boss extends Entity {
         this.direction = "down";
         this.state = 1;
 
-        this.hitPoint = 100;
+        this.hitPoint = 200;
     }
 
     public void getMobImage() {
