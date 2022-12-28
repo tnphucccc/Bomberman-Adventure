c9c4828537a6e70802e723b5152243d3a546def3
Nguyễn Tuấn Khoa <khoanguyen030803@gmail.com>
2022-12-28 13:19:44 +0700

    Add clear-mob-detection door

--- src/main/java/GUI/AssetSetter.java
+++ src/main/java/GUI/AssetSetter.java
@@ -19,9 +19,9 @@ public class AssetSetter {
         if(GameScene.getMapID() == 1) { // Mob Cord at Map 01
             GameScene.mobList.clear();
             GameScene.mobList.add(0, new Mob(4*Constant.TILE_SIZE, 5* Constant.TILE_SIZE));
-//            GameScene.mobList.add(1, new Mob(14*Constant.TILE_SIZE, 5*Constant.TILE_SIZE));
-//            GameScene.mobList.add(2, new Mob(14*Constant.TILE_SIZE, 8*Constant.TILE_SIZE));
-//            GameScene.mobList.add(3, new Mob(5*Constant.TILE_SIZE, 8*Constant.TILE_SIZE));
+            GameScene.mobList.add(1, new Mob(14*Constant.TILE_SIZE, 5*Constant.TILE_SIZE));
+            GameScene.mobList.add(2, new Mob(14*Constant.TILE_SIZE, 8*Constant.TILE_SIZE));
+            GameScene.mobList.add(3, new Mob(5*Constant.TILE_SIZE, 8*Constant.TILE_SIZE));
         } else if (GameScene.getMapID() == 2) { // Mob Cord at Map 02
             GameScene.mobList.clear();
             GameScene.mobList.add(0, new Mob(16 * Constant.TILE_SIZE, 7 * Constant.TILE_SIZE));
